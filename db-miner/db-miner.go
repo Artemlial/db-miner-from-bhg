9a654fb9d8bc9337b10196084986d64e6820aad2
Artem Lialin <zZ87111ff@gmail.com>
2023-11-01 14:19:39 +0300

    fixed structs and fields names, added logs to main

--- db-miner/db-miner.go
+++ db-miner/db-miner.go
@@ -6,10 +6,10 @@ import (
 )
 
 type DatabaseMiner interface {
-	GetScema() (*Scema, error)
+	GetSchema() (*Schema, error)
 }
 
-type Scema struct {
+type Schema struct {
 	Databases []Database
 }
 
@@ -19,24 +19,24 @@ type Database struct {
 }
 
 type Table struct {
-	Name     string
-	Columnns []string
+	Name    string
+	Columns []string
 }
 
-func Search(m DatabaseMiner) error{
-	s,err:= m.GetScema()
-	if err!=nil{
+func Search(m DatabaseMiner) error {
+	s, err := m.GetSchema()
+	if err != nil {
 		return err
 	}
 
-	re:=getRegex()
-	for _,database:=range s.Databases{
-		for _,table := range database.Tables{
-			for _,field:=range table.Columnns{
-				for _,r:=range re{
-					if r.MatchString(field){
+	re := getRegex()
+	for _, database := range s.Databases {
+		for _, table := range database.Tables {
+			for _, field := range table.Columns {
+				for _, r := range re {
+					if r.MatchString(field) {
 						fmt.Println(database)
-						fmt.Printf("[+] HIT: %s\n",field)
+						fmt.Printf("[+] HIT: %s\n", field)
 					}
 				}
 			}
@@ -45,7 +45,7 @@ func Search(m DatabaseMiner) error{
 	return nil
 }
 
-func getRegex() []*regexp.Regexp{
+func getRegex() []*regexp.Regexp {
 	return []*regexp.Regexp{
 		regexp.MustCompile(`(?i)social`),
 		regexp.MustCompile(`(?i)ssn`),
--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	// inner
 	"context"
-	"time"
+	"log"
 	"os"
+	"time"
 
 	// my modules
 	"dbMiner"
@@ -15,71 +16,76 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type MongoMiner struct{
-	Host string 
+type MongoMiner struct {
+	Host    string
 	session *mongo.Client
 }
 
-func New(host string) (*MongoMiner , error){
-	m:=MongoMiner{Host:host}
-	err:=m.connect()
-	if err!=nil{
-		return nil,err
+func New(host string) (*MongoMiner, error) {
+	m := MongoMiner{Host: host}
+	err := m.connect()
+	if err != nil {
+		log.Println("Func New: connecrtion error")
+		return nil, err
 	}
-	return &m,nil
+	return &m, nil
 }
 
-func (m *MongoMiner) connect() error{
-	ctx,cancel:=context.WithTimeout(context.TODO(),10*time.Second)
+func (m *MongoMiner) connect() error {
+	ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
 	defer cancel()
-	s,err:=mongo.Connect(ctx,options.Client().ApplyURI(m.Host))
-	if err!=nil{
+	s, err := mongo.Connect(ctx, options.Client().ApplyURI(m.Host))
+	if err != nil {
+		log.Println("Func connect: connection error:")
 		return err
 	}
 	m.session = s
 	return nil
 }
 
-func (m *MongoMiner) GetSchema() (*dbMiner.Schema,error){
+func (m *MongoMiner) GetSchema() (*dbMiner.Schema, error) {
 	var s = new(dbMiner.Schema)
 
-	dbnames,err:=m.session.ListDatabaseNames(context.TODO(),bson.D{})
-	if err!=nil{
-		return nil,err
+	dbnames, err := m.session.ListDatabaseNames(context.TODO(), bson.D{})
+	if err != nil {
+		log.Println("Func GetSchema: mongo.Client.ListDatabaseNames error:")
+		return nil, err
 	}
 
-	for _,dbname:=range dbnames{
-		db:=dbMiner.Database{Name:dbname,Tables:[]dbMiner.Table{}}
-		collections,err:=m.session.Database(dbname).ListCollectionNames(context.TODO(),bson.D{})
-		if err!=nil{
-			return nil,err
+	for _, dbname := range dbnames {
+		db := dbMiner.Database{Name: dbname, Tables: []dbMiner.Table{}}
+		collections, err := m.session.Database(dbname).ListCollectionNames(context.TODO(), bson.D{})
+		if err != nil {
+			log.Println("Func GetSchema: mongo.Database.ListCollectionNames error:")
+			return nil, err
 		}
-		for _,collection:=range collections{
-			table:=dbMiner.Table{Name:collection, Columns:[]string{}}
+		for _, collection := range collections {
+			table := dbMiner.Table{Name: collection, Columns: []string{}}
 
-			var doc bson.D 
-			err:=m.session.Database(dbname).Collection(collection).FindOne(context.TODO(),bson.D{}).Decode(&doc)
-			if err!=nil{
-				return nil,err
+			var doc bson.D
+			err := m.session.Database(dbname).Collection(collection).FindOne(context.TODO(), bson.D{}).Decode(&doc)
+			if err != nil {
+				log.Println("Func GetSchema: mongo.SingleResult error:")
+				return nil, err
 			}
 
-			for _,f :=range doc{
-				table.Columns = append(table.Columns,f.Key)
+			for _, f := range doc {
+				table.Columns = append(table.Columns, f.Key)
 			}
 
-			db.Tables = append(db.Tables,table)
+			db.Tables = append(db.Tables, table)
 		}
-		s.Databases = append(s.Databases,db)
+		s.Databases = append(s.Databases, db)
 	}
-	return s,nil
+	return s, nil
 }
 
 func main() {
-	mm,err:=New(os.Args[1])
-	if err!=nil{
+	mm, err := New(os.Args[1])
+	if err != nil {
 		panic(err)
 	}
-	if err:=dbMiner.Search(mm);err!=nil{
+	if err := dbMiner.Search(mm); err != nil {
 		panic(err)
 	}
 }
